6cb3ab3c68cf04f4e805ce868219757152433ff9
GonzaloPardoVillalibre <gonzalopmb@gmail.com>
2021-03-01 18:31:20 +0100

    add: utils to configure tensorflow graphics use

--- vicon/processing/utils/utils.py
+++ vicon/processing/utils/utils.py
@@ -207,3 +207,28 @@ def create_confusion_matrix(prediction:list, file_path:str, movements:list):
     sn.heatmap(metrics_df, annot=True, cmap='Blues', annot_kws={"size": 10}) # font size
     fig.tight_layout()
     plt.savefig(file_path + '/confusion-matrix-metrics.png')
+
+def restrict_to_physcial_gpu():
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        # Restrict TensorFlow to only use the first GPU
+        try:
+            tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+        except RuntimeError as e:
+            # Visible devices must be set before GPUs have been initialized
+            print(e)
+
+def set_memory_growth():
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
