cd3536884ebc4ec5f95eddac5ce51e847f617ae3
GonzaloPardoVillalibre <gonzalopmb@gmail.com>
2021-10-22 17:10:52 +0200

    refactor: interleaved dataframe

--- framework/pre-processing/unity/interleaved-dataframe/main.py
+++ framework/pre-processing/unity/interleaved-dataframe/main.py
@@ -1,34 +1,25 @@
 from toolz import interleave
 import os
 import pandas as pd
-import json
-import shutil 
+import json, sys
 
-# Porject main directory path
-# main_path = os.getcwd()
-main_path = '/TFG'
+# Current file path
 current_path = os.path.dirname(os.path.abspath(__file__))
-
-# Loads configuration file
-cfg_filename = main_path + '/framework/pre-processing/config.json'
-with open(cfg_filename) as f:
-  full_config = json.load(f)
-  cfg = full_config["in-dt"]
+interleaved_df_path = current_path + '/..'
+cfg_filename = interleaved_df_path + '/../config.json'
 
 # Path to _output file
-output_path = main_path + '/framework/pre-processing/interleaved-dataframe/_output/'
-zippedoutput_path = main_path + '/framework/pre-processing/interleaved-dataframe/_output-zipped/'
+output_path = interleaved_df_path + '/_output/'
+zippedoutput_path = interleaved_df_path + '/_output-zipped/'
 
-##################################################################
-# Methods                                                        #
-##################################################################
-def build_output_directory():
-  #Clean output directory
-  try:
-    shutil.rmtree(output_path)
-  except:
-    print("No folder had to be removed")
-  os.mkdir(output_path)
+# Load file utils file
+sys.path.append(current_path + '/../../utils')
+import filesUtils
+
+# Loads configuration file
+with open(cfg_filename) as f:
+  full_config = json.load(f)
+  cfg = full_config["unity-dt"]
 
 # Extracts orientation information for each line in csv
 def extract_quat_columns(file, jointName):
@@ -54,12 +45,12 @@ def extract_position_columns(file, jointName):
     df.columns = ['3D vecotr', '0', '1', '2']
     return df
 
-# Builds composed datatable for all sensors and one movement
+# Builds composed datatable for all sensors and one activities
 # Saves the new csv with the following data:
 #   - Rows: original * number of sensors in the list.
 #   - Columns: 4 in orientation sensors, 3 in position.
 def build_dt(sample: str, subject: str, movement: str):
-  subject_and_activity_file =  main_path + '/framework/pre-processing/dataset/' + subject + '-' + movement + '-' + sample + '.csv'
+  subject_and_activity_file =  interleaved_df_path + '/../dataset/' + subject + '-' + movement + '-' + sample + '.csv'
   orientation_df_list = list()
   position_df_list = list()
 
@@ -71,8 +62,8 @@ def build_dt(sample: str, subject: str, movement: str):
     print("      WARNING: File not accessible!")
     return
     
-  if cfg["orientationSensors"]["enabled"]: 
-    jointNamesListVicon = cfg["orientationSensors"]["list"]
+  if cfg["4D-Sensors"]["enabled"]: 
+    jointNamesListVicon = cfg["4D-Sensors"]["list"]
     for sensor in jointNamesListVicon:
         df = extract_quat_columns(subject_and_activity_file, sensor)
         orientation_df_list.append(df.values)
@@ -80,8 +71,8 @@ def build_dt(sample: str, subject: str, movement: str):
     subject_and_activity_file_output = output_path + subject + '-' + movement + '-Orientationjoints-' + sample + '.csv'
     final_df.to_csv(subject_and_activity_file_output, index=False)
 
-  if cfg["positionSensors"]["enabled"]: 
-    jointNamesListVicon = cfg["positionSensors"]["list"]
+  if cfg["3D-Sensors"]["enabled"]: 
+    jointNamesListVicon = cfg["3D-Sensors"]["list"]
     for sensor in jointNamesListVicon:
         df = extract_position_columns(subject_and_activity_file, sensor)
         position_df_list.append(df.values)
@@ -89,21 +80,19 @@ def build_dt(sample: str, subject: str, movement: str):
     subject_and_activity_file_output = output_path + subject + '-' + movement + '-Positionjoints-' + sample + '.csv'
     final_df.to_csv(subject_and_activity_file_output, index=False)
 
-# Analyzes configured movements and samples
+# Analyzes configured activities and samples
 def  create_dt_by_movement(subject: str):
   print("Building interleaved dataframes for subject: " + subject)
-  for movement in cfg["movements"]["list"]:
-    for sample in cfg["movements"]["samples"]:
+  for movement in cfg["activities"]["list"]:
+    for sample in cfg["activities"]["samples"]:
       print("   Movement " + movement + " & record sample " + sample)
       build_dt(sample, subject, movement)
 
+# Creates the so called "interleved dataframe" for Unity 3D representation.
 #########################
 # Main                  #
 #########################
-if cfg["enabled"]:
-  build_output_directory()
-  for subject in cfg["subjects"]["list"]:
-    create_dt_by_movement(subject)
-  print("\nINTERLEAVED DATAFRAMES BUILDING FINISHED")
-else:
-  print("\nnINTERLEAVED DATAFRAMES BUILDING DISABLED")
+filesUtils.build_output_directory(output_path)
+for subject in cfg["subjects"]["list"]:
+  create_dt_by_movement(subject)
+print("\nINTERLEAVED DATAFRAMES BUILDING FINISHED")
